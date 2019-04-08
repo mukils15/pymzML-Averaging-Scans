b94bc539d5e53620999357f9cfead2e37ca66724
Mukil Shanmugam <45906343+mukils15@users.noreply.github.com>
2019-04-07 21:14:28 -0700

    Add files via upload
    Changed from hardcoded file paths to command line prompting user for directory from which file paths were extracted.

--- UW-Multiprocessing.py
+++ UW-Multiprocessing.py
@@ -9,11 +9,26 @@ from multiprocessing import Pool
 import plotly.plotly as py
 import plotly.graph_objs as go
 import time
+import argparse
+import sys
 
-def getSpectralAverageAndWriteToFile(filepathPair):
+def create_arg_parser():
+    """"Creates and returns the ArgumentParser object."""
+
+    parser = argparse.ArgumentParser(description='Description of your app.')
+    parser.add_argument('inputDirectory',
+                    help='Path to the input directory.')
+    parser.add_argument('--outputDirectory',
+                    help='Path to the output that contains the resumes.')
+    #parser.add_argument('--numProcesses',
+                        #help='Number of processes used for multiprocessing')
+    #parser.add_argument('--chunkSize',
+                        #help='Chunk size used for multiprocessing map')
+    return parser
+
+
+def getSpectralAverageAndWriteToFile(filepath):
     #print ("Time when function actually called:" ,time.time())
-    filepath = filepathPair[0]
-    num = filepathPair[1]
     msrun = pymzml.run.Reader(filepath, MS_precisions={1:5e-6, 2:20e-6, 3:20e-6})
     # Get length of any one spectrum
     spec_length = len(msrun[2].mz)
@@ -40,81 +55,64 @@ def getSpectralAverageAndWriteToFile(filepathPair):
     # compute average intensity at each point across all spectra
     avg_i = [item / numspectra for item in total_i]
     mz_i_tuples = list(zip(spectrum.mz, avg_i))
-    # print (mz_i_tuples)
-    #trace = go.Scatter(x=spectrum.mz, y=avg_i)
-    #data = [trace]
-    #py.iplot(data, filename = filepath + ' .png')
     plt.plot(spectrum.mz, avg_i)
     plt.xlabel("m/z")
     plt.ylabel('Intensity')
 
-    # write spectra to another file
-    # run = pymzml.run.Reader(filepath, MS1_Precision = 5e-6, MSn_Precision = 20e-6)
-    # write_run = pymzml.run.Writer(filename='write_test.mzML', run=run , overwrite=True)
 
-    plt.savefig(filepath + str(num) + '.png')
+    plt.savefig(filepath + '.png')
 
 
 if __name__ == '__main__':
-    text_file_path = "C:\\Bruker Files"
-    mzml_file1 = os.path.join(text_file_path, "MS2-ESI-ISO.mzML")
-    mzml_file2 = os.path.join(text_file_path, "MS2-ESI-ISO2.mzML")
-    mzml_file3 = os.path.join(text_file_path, "MS3-ETD-CID.mzML")
+    ## Trying out the arg_parser to see if file paths can be passed from command-line
+    arg_parser = create_arg_parser()
+    parsed_args = arg_parser.parse_args(sys.argv[1:])
+    mzmlInputDir = parsed_args.inputDirectory
+    if os.path.exists(mzmlInputDir):
+        print("File exist")
+
+    mzmlFileArray = []
+    for root, dirs, files in os.walk(mzmlInputDir):
+        for file in files:
+            if file.endswith(".mzML"):
+                full_file_path = os.path.join(root,file)
+                print(full_file_path)
+                mzmlFileArray.append(full_file_path)
+
     x = 1
-    lowest_time = 100000;
+
+
     print ("CPU Count ", multiprocessing.cpu_count())
 
 
-    mzmlFileArray = []
-
-
-    fileArrayLen = 0
-    
-    while fileArrayLen < 100:
-        listToExtend = [[mzml_file1, fileArrayLen*3],[mzml_file2, fileArrayLen*3+1], [mzml_file3, fileArrayLen*3+2]]
-        mzmlFileArray.extend(listToExtend)
-        fileArrayLen += 1
-
-    #print(mzmlFileArray)
-
-    #exit
-    
-    #t_init = time.time()
-    #for index in range(len(mzmlFileArray)):
-    #   getSpectralAverageAndWriteToFile(mzmlFileArray[index])
-    #t_fin = time.time()
-    #avg_time = (t_fin-t_init)
-    #print("Average time for non-parallel processing: ", avg_time)
-    #print ("Average time per file: ", (avg_time/(len(mzmlFileArray))))
-
     while x<5:
-       p = Pool(x)
+        p = Pool(x)
 
-       t4 = time.time()
-       p.map(getSpectralAverageAndWriteToFile, mzmlFileArray)
-       t5 = time.time()
-       time_elapsed3 = t5-t4
-       print ("Time elapsed: ", time_elapsed3, " for ", x, "processes with no chunks")
-       print ("Average time per file: ", (time_elapsed3/(len(mzmlFileArray))))
+        t4 = time.time()
+        p.map(getSpectralAverageAndWriteToFile, mzmlFileArray)
+        t5 = time.time()
+        time_elapsed3 = t5-t4
+        print ("Time elapsed: ", time_elapsed3, " for",x, "processes with no chunks")
+        print ("Average time per file: ", (time_elapsed3/(len(mzmlFileArray))))
        
-       t0 = time.time()
-       p.map(getSpectralAverageAndWriteToFile, mzmlFileArray, 5)
-       t1 = time.time()
-       time_elapsed1 = t1-t0
-       print ("Time elapsed: ", time_elapsed1, " for ", x, "processes with chunk size=5")
-       print ("Average time per file: ", (time_elapsed1/(len(mzmlFileArray))))
+        t0 = time.time()
+        p.map(getSpectralAverageAndWriteToFile, mzmlFileArray, 5)
+        t1 = time.time()
+        time_elapsed1 = t1-t0
+        print ("Time elapsed: ", time_elapsed1, " for ", x, "processes with chunk size=5")
+        print ("Average time per file: ", (time_elapsed1/(len(mzmlFileArray))))
        
-       t2 = time.time()
-       p.map(getSpectralAverageAndWriteToFile, mzmlFileArray, 10)
-       t3 = time.time()
-       time_elapsed2 = t3-t2
-       print ("Time elapsed: ", time_elapsed2, " for ", x, "processes with chunk size=10")
-       print ("Average time per file: ", (time_elapsed2/(len(mzmlFileArray))))
+        t2 = time.time()
+        p.map(getSpectralAverageAndWriteToFile, mzmlFileArray, 10)
+        t3 = time.time()
+        time_elapsed2 = t3-t2
+        print ("Time elapsed: ", time_elapsed2, " for ", x, "processes with chunk size=10")
+        print ("Average time per file: ", (time_elapsed2/(len(mzmlFileArray))))
        
-       #if time_elapsed<lowest_time:
-       #    print("The new lowest time is ", time_elapsed, " for ", x, " processes")
+        #if time_elapsed<lowest_time:
+        #   print("The new lowest time is ", time_elapsed, " for ", x, " processes")
         #   lowest_time = time_elapsed
-       x = x+1
+        x = x+1
 
  
 
